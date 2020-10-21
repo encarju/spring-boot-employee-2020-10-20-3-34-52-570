c9262cd8baae152eba7de8115d8da30386dec4d6
bryanobmasca <bryanobmasca@gmail.com>
2020-10-21 20:39:59 +0800

    [RESTFULAPI Employees][JUSTINE/BRYAN] feature : Implement to update a company basic information

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -36,9 +36,24 @@ public class CompanyController {
     @GetMapping("/{companyId}/employees")
     public List<Employee> getEmployees(@PathVariable Integer companyId) {
         return companies.stream()
-                .filter(employee -> employee.getCompanyId().equals(companyId))
+                .filter(company -> company.getCompanyId().equals(companyId))
                 .findFirst()
                 .orElse(null).getEmployees();
     }
 
+    @PutMapping("/{companyId}")
+    public Company updateCompany(@PathVariable Integer companyId,  @RequestBody Company updatedCompany){
+        if(companies.stream()
+                .anyMatch(company -> company.getCompanyId().equals(companyId))){
+            companies.stream()
+                    .filter(company -> company.getCompanyId().equals(companyId))
+                    .findFirst()
+                    .ifPresent(company -> {
+                        companies.remove(company);
+                        companies.add(updatedCompany);
+                    });
+            return updatedCompany;
+        }
+        return null;
+    }
 }
