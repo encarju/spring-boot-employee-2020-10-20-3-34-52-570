8006f15a567fcbc71540c438451ce5407837f3f4
bryanobmasca <bryanobmasca@gmail.com>
2020-10-21 15:09:00 +0800

    [RESTFULAPI Employees][JUSTINE/BRYAN] feature : Add method get employees by gender

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -2,18 +2,11 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -22,40 +15,46 @@ public class EmployeesController {
 
     //Getting all employees
     @GetMapping
-    public List<Employee> getAll(){
+    public List<Employee> getAll() {
         return employees;
     }
 
     //Add an employee
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee addEmployee(@RequestBody Employee employee){
+    public Employee addEmployee(@RequestBody Employee employee) {
         employees.add(employee);
         return employee;
     }
 
     //Get specific employee
     @GetMapping("/{employeeId}")
-    public Employee getEmployee(@PathVariable Integer employeeId){
+    public Employee getEmployee(@PathVariable Integer employeeId) {
         return employees.stream().filter(employee -> employee.getId().equals(employeeId))
                 .findFirst().orElse(null);
 
     }
 
     @PutMapping("/{employeeId}")
-    public Employee updateEmployee(@PathVariable Integer employeeId,@RequestBody Employee employeeUpdate){
+    public Employee updateEmployee(@PathVariable Integer employeeId, @RequestBody Employee employeeUpdate) {
         employees.stream().filter(employee -> employee.getId().equals(employeeId))
                 .findFirst().ifPresent(employee -> {
-                    employees.remove(employee);
-                    employees.add(employeeUpdate);
+            employees.remove(employee);
+            employees.add(employeeUpdate);
         });
         return employeeUpdate;
     }
 
     @DeleteMapping("/{employeeId}")
-    public void removeEmployee(@PathVariable Integer employeeId){
+    public void removeEmployee(@PathVariable Integer employeeId) {
         employees.stream().filter(employee -> employee.getId().equals(employeeId))
                 .findFirst().ifPresent(employees::remove);
     }
 
+    @GetMapping(params = "gender")
+    public List<Employee> getByGender(@RequestParam("gender") String gender) {
+        return employees.stream()
+                .filter(employee -> employee.getGender().equalsIgnoreCase(gender))
+                .collect(Collectors.toList());
+    }
 }
