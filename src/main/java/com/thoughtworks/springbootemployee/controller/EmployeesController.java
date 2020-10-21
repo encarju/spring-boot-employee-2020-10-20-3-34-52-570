44c1d137be5cd40cf24fe281c29174e54e63d819
encarju <justine.encarnacion@oocl.com>
2020-10-21 14:49:55 +0800

    [RESTFULAPI Employees][JUSTINE/BRYAN] feature : Add method remove specific employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,12 @@ public class EmployeesController {
                     employees.add(employeeUpdate);
         });
         return employeeUpdate;
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public void removeEmployee(@PathVariable Integer employeeId){
+        employees.stream().filter(employee -> employee.getId().equals(employeeId))
+                .findFirst().ifPresent(employees::remove);
     }
 
 }
