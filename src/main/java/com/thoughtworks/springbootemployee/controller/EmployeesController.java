440491ff08c02971d8f6331278738e50ac1b516a
encarju <justine.encarnacion@oocl.com>
2020-10-21 14:35:33 +0800

    [RESTFULAPI Employees][JUSTINE][BRYAN] feature : Add method add an employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -1,8 +1,12 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,4 +23,11 @@ public class EmployeesController {
         return employees;
     }
 
+    //Add an employee
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee addEmployee(@RequestBody Employee employee){
+        employees.add(employee);
+        return employee;
+    }
 }
