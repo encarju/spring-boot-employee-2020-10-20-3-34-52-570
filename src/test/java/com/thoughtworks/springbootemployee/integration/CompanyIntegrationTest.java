fe54f97e73c62f4b31b1ee03da79f142a568af4b
encarju <justine.encarnacion@oocl.com>
2020-10-24 17:38:06 +0800

    [RESTFULAPI Employees][JUSTINE/JOHN] test : Add integration test case return company when create company

--- src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -9,13 +9,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CompanyIntegrationTest {
+    private static final String JUSTINE = "Justine";
+    private static final int AGE_23 = 23;
+    private static final String MALE = "Male";
+    private static final int SALARY = 2000000;
+    private static final String OOCL = "OOCL";
 
     @Autowired
     private CompanyRepository companyRepository;
@@ -41,6 +48,38 @@ public class CompanyIntegrationTest {
                 .andExpect(jsonPath("$[0].id").isNumber())
                 .andExpect(jsonPath("$[0].name").value("OOCL"))
                 .andExpect(jsonPath("$[0].employees").isArray());
+    }
+
+    @Test
+    public void should_create_company_when_create_given_company_request() throws Exception {
+        // given
+        String employeeJson = "{\n" +
+                "    \"name\" : \"" + OOCL + "\",\n" +
+                "    \"employees\" : [\n" +
+                "        {\n" +
+                "            \"name\" : \"" + JUSTINE + "\",\n" +
+                "            \"age\" : " + AGE_23 + ",\n" +
+                "            \"gender\" : \"" + MALE + "\",\n" +
+                "            \"salary\" : " + SALARY + "\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+
+        // when
+        // then
+        mockMvc.perform(post("/companies")
+                .contentType(APPLICATION_JSON)
+                .content(employeeJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.name").value(OOCL))
+                .andExpect(jsonPath("$.employees.length()").value(1))
+                .andExpect(jsonPath("$.employees[0].id").isNumber())
+                .andExpect(jsonPath("$.employees[0].name").value(JUSTINE))
+                .andExpect(jsonPath("$.employees[0].age").value(AGE_23))
+                .andExpect(jsonPath("$.employees[0].gender").value(MALE))
+                .andExpect(jsonPath("$.employees[0].salary").value(SALARY))
+                .andExpect(jsonPath("$.employees[0].companyId").isNumber());
     }
 
 }
