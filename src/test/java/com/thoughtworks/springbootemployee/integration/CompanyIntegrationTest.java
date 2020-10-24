82c0924c5a34ae167ce6fb3dac626f750e36a978
encarju <justine.encarnacion@oocl.com>
2020-10-25 01:18:52 +0800

    [RESTFULAPI Employees][JUSTINE/JOHN] refactor : Add setup method in CompanyIntegrationTest

--- src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -3,7 +3,9 @@ package com.thoughtworks.springbootemployee.integration;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,6 +23,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,6 +41,11 @@ public class CompanyIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Before
+    void setUp() {
+        companyRepository.deleteAll();
+    }
 
     @AfterEach
     void tearDown() {
@@ -165,6 +173,5 @@ public class CompanyIntegrationTest {
                 .andExpect(jsonPath("$[0].name").value(COSCO))
                 .andExpect(jsonPath("$[1].id").value(returnedCompanyId))
                 .andExpect(jsonPath("$[1].name").value(OOCL));
-
     }
 }
