4e2e5872dd6086920f28e137933f68754103f96c
encarju <justine.encarnacion@oocl.com>
2020-10-24 18:22:44 +0800

    [RESTFULAPI Employees][JUSTINE/JOHN] feature : Implement toResponse method in Company Mapper

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -1,13 +1,30 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.CompanyResponse;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.model.Company;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.beans.BeanUtils.copyProperties;
 
 @Component
 public class CompanyMapper {
 
     public CompanyResponse toResponse(Company company) {
-        return null;
+        EmployeeMapper employeeMapper = new EmployeeMapper();
+
+        CompanyResponse companyResponse = new CompanyResponse();
+        copyProperties(company, companyResponse);
+
+        List<EmployeeResponse> employeeResponses = company.getEmployees()
+                .stream()
+                .map(employeeMapper::toResponse)
+                .collect(toList());
+        companyResponse.setEmployeeResponses(employeeResponses);
+
+        return companyResponse;
     }
 }
