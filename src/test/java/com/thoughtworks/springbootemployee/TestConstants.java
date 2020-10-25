7f07ebdf91216ec690ecfe99dc319e00abd7eced
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-25 11:15:19 +0800

    [RESTFULAPI Employees][JUSTINE/JOHN] refactor : Arrange order and grouping of constants

--- src/test/java/com/thoughtworks/springbootemployee/TestConstants.java
+++ src/test/java/com/thoughtworks/springbootemployee/TestConstants.java
@@ -2,15 +2,18 @@ package com.thoughtworks.springbootemployee;
 
 public final class TestConstants {
 
-    public static final String OOCL = "OOCL";
     public static final String COSCO = "COSCO";
-    public static final String JUSTINE = "Justine";
+    public static final String OOCL = "OOCL";
+
     public static final int AGE_23 = 23;
-    public static final String MALE = "Male";
     public static final int SALARY = 233;
-    public static final String JOHN = "John";
+
     public static final String BRYAN = "Bryan";
+    public static final String JOHN = "John";
+    public static final String JUSTINE = "Justine";
     public static final String LILY = "Lily";
+
+    public static final String MALE = "Male";
     public static final String FEMALE = "Female";
 
     private TestConstants() {
