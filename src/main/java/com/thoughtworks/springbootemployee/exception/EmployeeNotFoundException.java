ad69b338b36b96ae6566c3e6afff369a4b6678ee
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-25 19:20:54 +0800

    [RESTFULAPI Employees][JUSTINE/JOHN] refactor : Rename parameter

--- src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeNotFoundException.java
+++ src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeNotFoundException.java
@@ -3,8 +3,7 @@ package com.thoughtworks.springbootemployee.exception;
 import static java.lang.String.format;
 
 public class EmployeeNotFoundException extends RuntimeException {
-
-    public EmployeeNotFoundException(int id) {
-        super(format("Employee with ID %d does not exist", id));
+    public EmployeeNotFoundException(int employeeId) {
+        super(format("Employee with ID %d does not exist", employeeId));
     }
 }
