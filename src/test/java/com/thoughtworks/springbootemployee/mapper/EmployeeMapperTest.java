04125a1a30264da57e22c312ed3760ca22db7f37
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-25 17:03:14 +0800

    [RESTFULAPI Employees][JUSTINE/JOHN] test : create test for EmployeeMapper.toResponse() that accepts a list

--- src/test/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapperTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapperTest.java
@@ -5,7 +5,11 @@ import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.thoughtworks.springbootemployee.TestHelper.AGE_23;
+import static com.thoughtworks.springbootemployee.TestHelper.JOHN;
 import static com.thoughtworks.springbootemployee.TestHelper.JUSTINE;
 import static com.thoughtworks.springbootemployee.TestHelper.MALE;
 import static com.thoughtworks.springbootemployee.TestHelper.SALARY;
@@ -50,5 +54,35 @@ class EmployeeMapperTest {
         assertEquals(AGE_23, employee.getAge());
         assertEquals(MALE, employee.getGender());
         assertEquals(SALARY, employee.getSalary());
+    }
+
+    @Test
+    void should_return_employee_responses_when_to_response_given_employees() {
+        //Given
+        Integer companyId = 1;
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee(1, JUSTINE, AGE_23, MALE, SALARY, companyId));
+        employeeList.add(new Employee(2, JOHN, AGE_23, MALE, SALARY, companyId));
+
+        //When
+        List<EmployeeResponse> employeeResponses = employeeMapper.toResponse(employeeList);
+
+        //Then
+        assertEquals(employeeList.size(), employeeResponses.size());
+
+        assertEquals(1, employeeResponses.get(0).getId());
+        assertEquals(JUSTINE, employeeResponses.get(0).getName());
+        assertEquals(AGE_23, employeeResponses.get(0).getAge());
+        assertEquals(MALE, employeeResponses.get(0).getGender());
+        assertEquals(SALARY, employeeResponses.get(0).getSalary());
+        assertEquals(companyId, employeeResponses.get(0).getCompanyId());
+
+        assertEquals(2, employeeResponses.get(1).getId());
+        assertEquals(JOHN, employeeResponses.get(1).getName());
+        assertEquals(AGE_23, employeeResponses.get(1).getAge());
+        assertEquals(MALE, employeeResponses.get(1).getGender());
+        assertEquals(SALARY, employeeResponses.get(1).getSalary());
+        assertEquals(companyId, employeeResponses.get(1).getCompanyId());
     }
 }
