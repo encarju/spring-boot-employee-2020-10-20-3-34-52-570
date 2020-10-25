89b38e10d91d2d17921d16cb774bbc0ab610bef8
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-25 18:41:34 +0800

    [RESTFULAPI Employees][JUSTINE/JOHN] refactor : Utilize Mapping in toResponse()

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -2,7 +2,6 @@ package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.CompanyRequest;
 import com.thoughtworks.springbootemployee.dto.CompanyResponse;
-import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.model.Company;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,27 +9,13 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-import static com.thoughtworks.springbootemployee.mapper.EmployeeMapper.EMPLOYEE_MAPPER;
-import static java.util.stream.Collectors.toList;
-import static org.springframework.beans.BeanUtils.copyProperties;
-
 @Mapper
 public interface CompanyMapper {
 
     CompanyMapper COMPANY_MAPPER = Mappers.getMapper(CompanyMapper.class);
 
-    default CompanyResponse toResponse(Company company) {
-        CompanyResponse companyResponse = new CompanyResponse();
-        copyProperties(company, companyResponse);
-
-        List<EmployeeResponse> employeeResponses = company.getEmployees()
-                .stream()
-                .map(EMPLOYEE_MAPPER::toResponse)
-                .collect(toList());
-        companyResponse.setEmployees(employeeResponses);
-
-        return companyResponse;
-    }
+    @Mapping(source = "company.employees", target = "employees")
+    CompanyResponse toResponse(Company company);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "employees", ignore = true)
