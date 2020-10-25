3b590ed03a3683ad887dc41cf9dae7d00b73f668
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-25 11:17:49 +0800

    [RESTFULAPI Employees][JUSTINE/JOHN] refactor : Create new constants

--- src/test/java/com/thoughtworks/springbootemployee/TestConstants.java
+++ src/test/java/com/thoughtworks/springbootemployee/TestConstants.java
@@ -2,10 +2,13 @@ package com.thoughtworks.springbootemployee;
 
 public final class TestConstants {
 
+    public static final String ALIBABA = "Alibaba";
+    public static final String ALIBABAS = "Alibabas";
     public static final String COSCO = "COSCO";
     public static final String OOCL = "OOCL";
 
     public static final int AGE_23 = 23;
+    public static final int ONCE = 1;
     public static final int SALARY = 233;
 
     public static final String BRYAN = "Bryan";
